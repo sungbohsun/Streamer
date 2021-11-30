585c8ed9fc6246f6b201747ab7469dfe3ecf6aca
sung <sung@LAPTOP-JCNOIRK6>
2021-11-30 08:36:53 +0800

    加入專案檔案。

---
+++ Streamer/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Streamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.captureButton = new System.Windows.Forms.Button();
+            this.captureImageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(9, 18);
+            this.captureButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(58, 32);
+            this.captureButton.TabIndex = 0;
+            this.captureButton.Text = "Start";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
+            // captureImageBox
+            // 
+            this.captureImageBox.Location = new System.Drawing.Point(79, 18);
+            this.captureImageBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.captureImageBox.Name = "captureImageBox";
+            this.captureImageBox.Size = new System.Drawing.Size(840, 640);
+            this.captureImageBox.TabIndex = 1;
+            this.captureImageBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 820);
+            this.Controls.Add(this.captureImageBox);
+            this.Controls.Add(this.captureButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button captureButton;
+        private System.Windows.Forms.PictureBox captureImageBox;
+    }
+}
+
---
+++ Streamer/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Emgu.CV;
+using System;
+using System.Windows.Forms;
+using Capture = Emgu.CV.Capture;
+
+namespace Streamer
+
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture = null;
+        private bool _captureInProgress;
+        public Form1()
+        {
+            InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                //_capture = new Capture("rtsp://帳號:密碼@10.248.63.220/");//
+                _capture = new Capture("rtsp://192.168.13.205:5556/unicast");
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            try
+            {
+                Mat frame = new Mat();
+                _capture.Retrieve(frame, 0);
+                captureImageBox.Image = frame.Bitmap;
+            }
+            catch (Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Text = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Text = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+    }
+}
